339e9368a5d85e51f51e8dce30a6efd15e4b2cd9
Luis Tiago <luistiago.andrighetto@gmail.com>
2025-03-26 02:33:26 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+@app.route("/mp-proxy", methods=["POST"])
+def mp_proxy():
+    try:
+        print("Notificação recebida:", request.json)
+
+        # Reenvia para o seu endpoint no PythonAnywhere
+        response = requests.post(
+            "http://digitalstoregames.pythonanywhere.com/notification3",
+            json=request.json,
+            timeout=5
+        )
+
+        print("Resposta do seu servidor:", response.status_code)
+        return {"status": "forwarded"}, 200
+    except Exception as e:
+        print("Erro:", e)
+        return {"error": str(e)}, 500
